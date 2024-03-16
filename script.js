348969a2e882286eff6f2ce96705031cfee40cf7
BAOUCHbaSalma <baouch.salma0@gmail.com>
2024-03-16 21:14:55 +0100

    Array of questions and answers

---
+++ script.js
@@ -0,0 +1,38 @@
+const quiz=[
+    {
+       Question:"Q.1 What is a correct syntax to output 'Hello World' in Java?",
+       Option:[
+          ' echo("Hello World")',
+           'print("Hello World")',
+           'System.out.print("Hello World")',
+           'Console.WriteLine("Hello World")'
+
+       ],
+       Correcte:'System.out.print("Hello World")'
+    },
+
+   {
+       Question:"Q.2 How do you create a variable with the numeric value 5?",
+       Option:[
+          ' x = 5',
+           'int x = 5',
+           'num x = 5',
+           'float x = 5'
+
+       ],
+       Correcte:'int x = 5'
+   },
+
+   {
+       Question:"Q.3 Which operator is used to add together two values?",
+       Option:[
+          ' The & sign',
+           'The * sign',
+           'The + sign',
+
+       ],
+       Correcte:'The + sign'
+   },
+
+
+]
